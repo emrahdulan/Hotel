b6dc510cee0dcf3d1897203c451e6488002b60d3
Ajdin8107 <ajdin_8@hotmail.com>
2022-01-23 20:53:36 +0100

    Odradjen dio posla

---
+++ Hotel.WebAPI/Dto/RoomDto/RoomDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Hotel.WebAPI.Dto.RoomDto
+{
+    public class RoomDto
+    {
+        public int Id { get; set; }
+        public int HotelID { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
---
+++ Hotel.WebAPI/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Hotel.WebAPI.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int HotelID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Salary { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Updated { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
